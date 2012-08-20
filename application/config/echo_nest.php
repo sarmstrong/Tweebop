8f9b474425c94414c1b21452f9ee5cb75a7f3ac4
Steve Armstrong <sarmstrongmusic@gmail.com>
2012-08-19 20:36:35 -0400

    Modified environment for delpoying on Pagoda. Added back in config files and an empty cache directory. Also created a boxfile

---
+++ application/config/echo_nest.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['echo_nest_key'] = 'WG7HBALBAFPSGYB2N';
+
+$config['cron_key'] = '59257c474f305b41665535255420562e'; 
